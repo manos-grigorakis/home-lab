9aca4c0740cd29341222ae50a5371997f8dd4948
manos <manos.grigorakis03@gmail.com>
2026-04-14 16:56:22 +0300

    chore: enable S3 versioning instead of dynamodb

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "amzn-s3-homelab-terraform-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "dynamodb-state-locking-terraform"
+    bucket       = "amzn-s3-homelab-terraform-bucket"
+    key          = "dev/terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
   }
 }
